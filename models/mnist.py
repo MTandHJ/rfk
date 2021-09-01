db4885a414da635dda527c51c0ecd195f88bbf13
MTandHJ <congxueric@gmail.com>
2021-09-01 16:48:31 +0800

    modify models.MNIST

--- models/mnist.py
+++ models/mnist.py
@@ -5,7 +5,6 @@
 
 
 
-import torch
 import torch.nn as nn
 from .base import ADArch
 from .layerops import Sequential
@@ -13,41 +12,44 @@ from .layerops import Sequential
 
 
 class MNIST(ADArch):
-
-    def __init__(self, dim_feature=200, num_classes=10):
+    def __init__(self, dim_features=200, num_classes=10, drop=0.5):
         super(MNIST, self).__init__()
 
-        self.conv = Sequential( # 1 x 28 x 28
-            nn.Conv2d(1, 32, 3),   # 32 x 26 x 26
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, 3),  # 32 x 24 x 24
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),       # 32 x 12 x 12
-            nn.Conv2d(32, 64, 3),  # 64 x 10 x 10
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3),  # 64 x 8 x 8
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2)        # 64 x 4 x 4
+        self.conv = Sequential(
+            nn.Conv2d(1, 32, 3),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
         )
 
         self.dense = Sequential(
             nn.Linear(64 * 4 * 4, 200),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(200),
-            nn.Linear(200, dim_feature),
-            nn.BatchNorm1d(dim_feature)
+            nn.ReLU(True),
+            nn.Dropout(drop),
+            nn.Linear(200, dim_features)
         )
-        self.activation = nn.ReLU(inplace=True)
-        self.fc = nn.Linear(dim_feature, num_classes)
+        self.activation = nn.ReLU(True)
+        self.fc = nn.Linear(dim_features, num_classes)
 
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d)):
+                nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        nn.init.constant_(self.fc.weight, 0)
+        nn.init.constant_(self.fc.bias, 0)
 
     def forward(self, x):
         x = self.conv(x).flatten(start_dim=1)
         features = self.activation(self.dense(x))
-        outs = self.fc(features)
-        return outs
-
+        logits = self.fc(features)
+        return logits
