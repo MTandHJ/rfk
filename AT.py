3160539d5915d0d4802802dd2ed25cc360a6db9b
MTandHJ <congxueric@gmail.com>
2021-10-11 20:56:12 +0800

    eval-freq:1

--- AT.py
+++ AT.py
@@ -51,7 +51,7 @@ parser.add_argument("--ratio", type=float, default=.0,
 # eval
 parser.add_argument("--eval-train", action="store_true", default=False)
 parser.add_argument("--eval-valid", action="store_false", default=True)
-parser.add_argument("--eval-freq", type=int, default=5,
+parser.add_argument("--eval-freq", type=int, default=1,
                 help="for valid dataset only")
 
 parser.add_argument("--resume", action="store_true", default=False)
