e8dc5c2e09b588955d9f2fbe54101623cc8f2590
MTandHJ <congxueric@gmail.com>
2021-11-04 19:08:02 +0800

    Update ALP

--- ALP.py
+++ ALP.py
@@ -36,7 +36,7 @@ parser.add_argument("-beta1", "--beta1", type=float, default=0.9,
                 help="the first beta argument for Adam")
 parser.add_argument("-beta2", "--beta2", type=float, default=0.999,
                 help="the second beta argument for Adam")
-parser.add_argument("-wd", "--weight_decay", type=float, default=2e-4,
+parser.add_argument("-wd", "--weight_decay", type=float, default=5e-4,
                 help="weight decay")
 parser.add_argument("-lr", "--lr", "--LR", "--learning_rate", type=float, default=0.1)
 parser.add_argument("-lp", "--learning_policy", type=str, default="Pang2021ICLR", 
